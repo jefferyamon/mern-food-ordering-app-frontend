0be6461803931142ea494538e59a7a77cfe12ef5
unknown <jefferyamon2468@gmail.com>
2025-03-04 13:55:16 +0000

    added search page and filters

---
+++ src/components/SearchResultCard.tsx
@@ -0,0 +1,52 @@
+import { Restaurant } from "@/type";
+import { Link } from "react-router";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { Banknote, Clock, Dot } from "lucide-react";
+
+type Props = {
+  restaurant: Restaurant;
+};
+
+const SearchResultCard = ({ restaurant }: Props) => {
+  return (
+    <Link
+      to={`/detail/${restaurant._id}`}
+      className="grid grid-cols-[2fr_3fr] gap-5 group"
+    >
+      <AspectRatio ratio={16 / 6}>
+        <img
+          src={restaurant.imageUrl}
+          className="rounded-md w-full h-full object-cover"
+        />
+      </AspectRatio>
+      <div>
+        <h3 className="text-2xl font-bold tracking-tight mb-2 group-hover:underline">
+          {restaurant.restaurantName}
+        </h3>
+        <div className="grid md:grid-cols-1 gap-2" id="card-content">
+          <div className="flex flex-row flex-wrap">
+            {restaurant.cuisines.map((item, index) => {
+              return (
+                <span className="flex">
+                  <span>{item}</span>
+                  {index < restaurant.cuisines.length - 1 && <Dot />}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex gap-2 flex-col">
+          <div className="flex items-center gap-1 text-green-600">
+            <Clock className="text-green-600" />
+            {restaurant.estimatedDeliveryTime} mins
+          </div>
+          <div className="flex items-center gap-1">
+            <Banknote />
+            Delivery from ${(restaurant.deliveryPrice / 100).toFixed(2)}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+export default SearchResultCard;
